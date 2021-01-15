9f713c428780de268d801b865a75671fcd48fe09
yu-chenxi <makecodechangeword@outlook.com>
2021-01-16 02:27:56 +0800

    no commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-fn f(mut s: String) -> String {
-    s.push_str("world!");
-    s
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+#[derive(Debug, Clone, Copy)]
+struct Pt {
+    x: i32,
+    y: i32,
+}
+
+impl Pt {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
 }
 
 fn main() {
-    let mut s = String::from("hello ");
-    s = f(s);
-    println!("s = {}", s);
+    let pt = Arc::new(Mutex::new(Pt::new(1, 2)));
+    const NUM: u32 = 3;
+    let mut ts = vec![];
+    for _ in 0..NUM {
+        let pt = Arc::clone(&pt);
+        // multi-threads point to single memory-area
+        let t = thread::spawn(move || {
+            let mut pt = pt.lock().unwrap();
+            pt.x += 1; // ptr
+            pt.y += 2;
+        });
+        ts.push(t);
+    }
+
+    for t in ts {
+        t.join().unwrap();
+    }
+
+    println!("{:?}", pt.lock().unwrap());
 }
