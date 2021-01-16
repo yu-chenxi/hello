713971368d185fff91cfd590b6b3537387726f0b
yu-chenxi <makecodechangeword@outlook.com>
2021-01-17 03:20:25 +0800

    hello drone

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
-use std::panic;
-
-fn _my_div(x: i32, y: i32) -> Result<i32, String> {
-    if y != 0 {
-        Ok(x / y)
-    } else {
-        Err(String::from("div 0!"))
+fn sort() {
+    const N: usize = 1000000;
+    let mut v = Vec::with_capacity(N);
+    for _ in 0..N {
+        v.push(rand::random::<i32>() % (N as i32));
     }
-}
-
-fn my_div() {
-    let x = 3;
-    let mut y = 1;
-    println!("{}", _my_div(x, y).unwrap());
-    y = 0;
-    println!("{}", _my_div(x, y).unwrap());
+    v.sort();
+    print!("{:?}", &v[..10]);
 }
 
 fn main() {
-    let res = panic::catch_unwind(|| {
-        my_div();
-    });
-
-    if let Err(_) = res {}
-    println!("catch the panic!");
+    sort();
 }
