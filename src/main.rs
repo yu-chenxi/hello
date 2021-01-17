ef8f9970e62b217218f4fe6f2c84f9bb643c3ab6
yu-chenxi <makecodechangeword@outlook.com>
2021-01-17 16:44:30 +0800

    impl += and def for Pt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-fn sort() {
-    const N: usize = 1000000;
-    let mut v = Vec::with_capacity(N);
-    for _ in 0..N {
-        v.push(rand::random::<i32>() % (N as i32));
+use std::ops::AddAssign;
+
+#[derive(Debug, Clone, Copy)]
+struct Pt {
+    x: i32,
+    y: i32,
+}
+
+impl AddAssign for Pt {
+    fn add_assign(&mut self, rhs: Self) {
+        // todo!()
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
-    v.sort();
-    println!("{:?}", &v[..10]);
+}
+
+impl Default for Pt {
+    fn default() -> Self {
+        // todo!()
+        Pt { x: -1, y: -1 }
+    }
 }
 
 fn main() {
-    sort();
+    let mut pt1 = Pt { x: 1, y: 2 };
+    let pt2 = Pt::default();
+    pt1 += pt2;
+    println!("pt1 = {:?}, pt2 = {:?}", pt1, pt2);
 }
