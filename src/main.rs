8a676505ddacefebb9b5bdec4299d0d3f9c92003
yu-chenxi <makecodechangeword@outlook.com>
2021-01-18 01:22:15 +0800

    no comment2

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,57 @@
-use std::ops::AddAssign;
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-struct Pt {
-    x: i32,
-    y: i32,
-}
-
-impl AddAssign for Pt {
-    fn add_assign(&mut self, rhs: Self) {
-        // todo!()
-        self.x += rhs.x;
-        self.y += rhs.y;
-    }
-}
-
-impl Default for Pt {
-    fn default() -> Self {
-        // todo!()
-        Pt { x: -1, y: -1 }
-    }
-}
-
 fn main() {
-    let mut pt1 = Pt { x: 1, y: 2 };
-    let pt2 = Pt::default();
-    pt1 += pt2;
-    println!("pt1 = {:?}, pt2 = {:?}", pt1, pt2);
+    let v: Vec<i32> = vec![1, 2, 3];
+    println!("{:?}", v);
+    let s: i32 = v.iter().sum();
+    println!("{}", s);
+    let v2: Vec<i32> = v.iter().map(|x| x * x).collect();
+    println!("{:?}", v2);
+    let s = v.iter().fold(1, |x, y| 2 * x + y);
+    // f(-1) = 1
+    // f(x+1) = 2 * f(x) + v[x+1]
+    // f(0) = 2 * 1 + 1 = 3
+    // f(1) = 2 * 3 + 2 = 8
+    // f(2) = 2 * 8 + 3 = 19
+    println!("{}", s);
     foo();
-    bar();
-    // let b: bool = rand::random();
-    // println!("b = {}", b);
+}
+
+trait Fly {
+    fn fly(&self);
+}
+
+struct Duck;
+struct Pig;
+
+impl Fly for Duck {
+    fn fly(&self) {
+        // todo!()
+        println!("duck can fly.");
+    }
+}
+
+impl Fly for Pig {
+    fn fly(&self) {
+        // todo!()
+        println!("pig can't fly.");
+    }
+}
+
+fn static_dispatch<T>(t: &T)
+where
+    T: Fly,
+{
+    t.fly();
+}
+
+fn dynamic_dispatch(t: &dyn Fly) {
+    t.fly();
 }
 
 fn foo() {
-    // debug_assert_eq!(Pt { x: -1, y: 0 }, Pt::default()); // not be useful in release mode
-}
-
-fn bar() {
-    // type MyRes = Result<i32, String>;
-    // let x: MyRes = Err(String::from("a error"));
-    // x.unwrap();
-    // x.expect("expect i32 type");
-    // assert_eq!(atcual, expected)
-    // let x: MyRes = Ok(13);
-    // x.unwrap_err(); // panic if Ok
+    let duck = Duck;
+    let pig = Pig;
+    static_dispatch(&duck);
+    static_dispatch(&pig);
+    dynamic_dispatch(&duck); // &dyn Fly = T* + vtable
+    dynamic_dispatch(&pig);
 }
