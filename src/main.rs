2496d07fb39b0f991952f708ceb064fede95865f
yu-chenxi <makecodechangeword@outlook.com>
2021-01-19 01:01:23 +0800

    no comment

--- src/main.rs
+++ src/main.rs
@@ -259,6 +259,7 @@ fn main() {
     foo18();
     foo19();
     foo20();
+    foo21();
 }
 
 fn foo20() {
@@ -295,4 +296,62 @@ fn foo() {
     fn f() {
         println!("2");
     }
+}
+
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+struct CircleBuilder {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+
+    fn new() -> CircleBuilder {
+        CircleBuilder {
+            x: 0.0,
+            y: 0.0,
+            radius: 1.0,
+        }
+    }
+}
+
+impl CircleBuilder {
+    fn x(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.x = coordinate;
+        self
+    }
+
+    fn y(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.y = coordinate;
+        self
+    }
+
+    fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+        self.radius = radius;
+        self
+    }
+
+    fn build(&self) -> Circle {
+        Circle {
+            x: self.x,
+            y: self.y,
+            radius: self.radius,
+        }
+    }
+}
+
+fn foo21() {
+    let c = Circle::new().x(1.0).y(2.0).radius(2.0).build();
+    println!("{}", c.x);
+    println!("{}", c.y);
+    println!("{:.6}", c.area());
 }
